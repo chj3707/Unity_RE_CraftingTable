4422f95fed953211ff8bdb149b04109a58a17c8f
HYUNJIN <62164992+chj3707@users.noreply.github.com>
2022-06-02 23:42:40 +0900

    아이템 추가 버튼 수정 완료, 드래그 아이템 슬롯 클래스 상속 하여 사용

--- Assets/02.Scripts/Inventory.cs
+++ Assets/02.Scripts/Inventory.cs
@@ -10,7 +10,6 @@ public class Inventory : MonoBehaviour
     public int m_SlotCount;                                                // 슬롯 복사할 개수
 
     private static LinkedList<Slot> m_SlotList = new LinkedList<Slot>();   // 복사한 슬롯 데이터 리스트
-    private static LinkedListNode<Slot> m_CurrentNode;                     // 링크드 리스트 노드 접근용 변수
 
     void Start()
     {
@@ -21,34 +20,37 @@ public class Inventory : MonoBehaviour
     // 인벤토리에 재료 아이템 추가
     public static void AddItemToInventory(Item p_item)
     {
-        IEnumerator<Slot> enumerator = m_SlotList.GetEnumerator();  // 리스트 열거자 선언
-        int ItemStack = 0;
+        // 리스트 열거자 선언
+        IEnumerator<Slot> enumerator = m_SlotList.GetEnumerator();
+        // 슬롯에 채울 아이템 개수
+        int itemStack = p_item.m_IsStackable ? MaxItemStack.Stackable : MaxItemStack.NonStackable;
 
         while (enumerator.MoveNext())
         {
-            Slot currSlot = enumerator.Current;                     // 현재 확인할 슬롯
-            Item currItem = currSlot.GetItemInfo();                 // 슬롯에 있는 아이템 정보
-            if (ItemStack == 0)
-                ItemStack = p_item.m_IsStackable ? MaxItemStack.Stackable : MaxItemStack.NonStackable;
-
+            Slot currSlot = enumerator.Current;              // 현재 확인할 슬롯
+            Item currItem = currSlot.GetItemInfo(currSlot);  // 슬롯에 있는 아이템 정보
             /* 
              *  재료 아이템 추가 조건
              *  1.슬롯이 비어 있다.
              *  2.추가할 아이템과 같은 아이템 이고, 아이템 스택이 가득 차 있지 않다.
              */
-            if (currItem == null ||
-                currItem == p_item && !currSlot.IsItemStackFull())
+            if (currItem == null)
             {
-                while(!currSlot.IsItemStackFull())
-                {
-                    currSlot.m_ItemStack.Push(p_item);  // 슬롯에 재료 아이템 추가
-                    --ItemStack;
-                    if (ItemStack == 0) break;
-                }
+                for (int i = 0; i < itemStack; i++)
+                    currSlot.m_ItemStack.Push(p_item); 
 
-                currSlot.UpdateUI();                    // 슬롯 UI 업데이트
-                if(ItemStack == 0) break;
+                itemStack = 0;
             }
+            else if(currItem == p_item)
+            {
+                while (!currSlot.IsItemStackFull(currSlot) && itemStack > 0)
+                {
+                    currSlot.m_ItemStack.Push(p_item);
+                    --itemStack;
+                }
+            }
+            currSlot.UpdateUI(currSlot);                   // 슬롯 UI 업데이트
+            if (itemStack == 0) break;                     // 채울 아이템 개수가 0이면 종료
         }
     }
 }
--- Assets/02.Scripts/Slot/Slot.cs
+++ Assets/02.Scripts/Slot/Slot.cs
@@ -10,77 +10,79 @@ public static class MaxItemStack
     public static int NonStackable = 1;
 }
 
+/*
+ *  슬롯 관리 스크립트 
+ */
 public class Slot : MonoBehaviour, IPointerClickHandler
 {
     public Stack<Item> m_ItemStack = new Stack<Item>();  // 슬롯에 있는 아이템 정보
-    public Image m_ItemImage = null;                     // 아이템 이미지
-    public Text m_ItemCount = null;                      // 아이템 개수
+    protected Image m_ItemImage = null;                  // 아이템 이미지
+    protected Text m_ItemCount = null;                   // 아이템 개수
 
     private void Awake()
     {
+        this.Initialize();
+    }
+
+    // 초기화
+    protected virtual void Initialize()
+    {
         m_ItemImage = transform.GetChild(0).GetComponent<Image>();
         m_ItemCount = m_ItemImage.GetComponentInChildren<Text>();
-        
+
         m_ItemImage.enabled = false;
         m_ItemCount.enabled = false;
     }
 
     // 슬롯 변경 내용 있을 때마다 호출하여 UI 업데이트
-    public void UpdateUI()
+    public void UpdateUI(Slot p_slot)
     {
         // 슬롯이 비었을 때
-        if (this.IsSlotEmpty())
+        if (IsSlotEmpty(p_slot))
         {
-            m_ItemImage.sprite = null;
-            m_ItemImage.enabled = false;
+            p_slot.m_ItemImage.sprite = null;
+            p_slot.m_ItemImage.enabled = false;
 
-            m_ItemCount.text = m_ItemStack.Count.ToString();
-            m_ItemCount.enabled = false;
+            p_slot.m_ItemCount.text = p_slot.m_ItemStack.Count.ToString();
+            p_slot.m_ItemCount.enabled = false;
             return;
         }
 
         // 슬롯에 아이템이 있을 때
-        Item currItem = m_ItemStack.Peek();
+        Item currItem = p_slot.m_ItemStack.Peek();
 
-        m_ItemImage.enabled = true;
-        m_ItemImage.sprite = currItem.m_ItemSprite;
+        p_slot.m_ItemImage.enabled = true;
+        p_slot.m_ItemImage.sprite = currItem.m_ItemSprite;
         // 쌓을 수 있는 아이템만 개수 표시
-        if (currItem.m_IsStackable) m_ItemCount.enabled = true;
-        m_ItemCount.text = m_ItemStack.Count.ToString();
-    }
-
-    // 슬롯 정보 초기화
-    public void ResetSlot()
-    {
-        this.m_ItemStack.Clear();
-        this.UpdateUI();
+        if (currItem.m_IsStackable) p_slot.m_ItemCount.enabled = true;
+        p_slot.m_ItemCount.text = p_slot.m_ItemStack.Count.ToString();
     }
 
     // 슬롯에 쌓인 아이템 개수 가져가는 용도
-    public int GetItemStackCount()
+    public int GetItemStackCount(Slot p_slot)
     {
-        return m_ItemStack.Count;
+        return p_slot.m_ItemStack.Count;
     }
 
     // 슬롯에 있는 아이템 정보 가져가는 용도
-    public Item GetItemInfo()
+    public Item GetItemInfo(Slot p_slot)
     {
-        return this.IsSlotEmpty() ? null : m_ItemStack.Peek();
+        return IsSlotEmpty(p_slot) ? null : p_slot.m_ItemStack.Peek();
     }
 
     // 슬롯에 있는 아이템 스택이 가득 찼는지 확인
-    public bool IsItemStackFull()
+    public bool IsItemStackFull(Slot p_slot)
     {
-        Item tempItem = m_ItemStack.Peek();
-        if (tempItem.m_IsStackable && m_ItemStack.Count == MaxItemStack.Stackable) return true;
-        else if(!tempItem.m_IsStackable && m_ItemStack.Count == MaxItemStack.NonStackable) return true;
+        Item tempItem = p_slot.m_ItemStack.Peek();
+        if (tempItem.m_IsStackable && p_slot.m_ItemStack.Count == MaxItemStack.Stackable) return true;
+        else if(!tempItem.m_IsStackable && p_slot.m_ItemStack.Count == MaxItemStack.NonStackable) return true;
         return false;
     }
 
     // 슬롯이 비어 있는지 확인
-    public bool IsSlotEmpty()
+    public bool IsSlotEmpty(Slot p_slot)
     {
-        if (m_ItemStack.Count == 0) return true;
+        if (p_slot.m_ItemStack.Count == 0) return true;
         return false;
     }
 
@@ -93,19 +95,19 @@ public class Slot : MonoBehaviour, IPointerClickHandler
      * 2-2. 슬롯 우 클릭 :: 드래그 중인 아이템 슬롯에 1개 드랍
      * 
      * 3. 드래그 중이 아닐 때 아이템이 있는 슬롯 클릭
-     * 3-1. 슬롯 좌 클릭 :: 슬롯에 있는 모든 아이템 들기
-     * 3-2. 슬롯 우 클릭 :: 슬롯에 있는 아이템 절반(올림) 들기
+     * 3-1. 슬롯 좌 클릭 :: 슬롯에 있는 모든 아이템 드래그
+     * 3-2. 슬롯 우 클릭 :: 슬롯에 있는 아이템 절반(올림) 드래그
      * 
      * 4. 드래그 중에 아이템이 있는 슬롯 클릭
      * 4-1. 슬롯에 있는 아이템과 드래그 중인 아이템이 다른 경우 :: 아이템 스왑
-     * 4-2. 아이템이 같은 경우 슬롯 좌 클릭 :: 슬롯의 아이템 최대 개수 만큼 채우고 나머지 들기
+     * 4-2. 아이템이 같은 경우 슬롯 좌 클릭 :: 슬롯의 아이템 최대 개수 만큼 채우고 나머지 드래그
      * 4-3. 아이템이 같은 경우 슬롯 우 클릭 :: 슬롯의 아이템이 최대 개수가 아니면 1개 채우기
      * 
      */
     public void OnPointerClick(PointerEventData eventData)
     {
         EventManager eventManager = EventManager.GetInstance;
-        switch(this.IsSlotEmpty())
+        switch(this.IsSlotEmpty(this))
         {
             // 빈 슬롯 클릭
             case true:
@@ -113,7 +115,7 @@ public class Slot : MonoBehaviour, IPointerClickHandler
                 {
                     // 아이템 드래그중 :: 아이템 드랍
                     case true:
-                        DropTheItem(eventData, eventManager);
+                        ItemsDrop(eventData, eventManager);
                         break;
 
                     // 아이템 드래그중이 아님 :: return
@@ -126,33 +128,34 @@ public class Slot : MonoBehaviour, IPointerClickHandler
             case false:
                 switch (eventManager.m_IsDragging)
                 {
-                    // 드래그 중 :: 슬롯에 있는 아이템과 드래그 중인 아이템 정보 스왑 또는 드랍(아이템이 같은 경우)
+                    // 드래그 중 :: 슬롯에 있는 아이템과 드래그 중인 아이템 정보 스왑, 드랍(아이템이 같은 경우)
                     case true:
+                        ItemsSwapOrDrop(eventData, eventManager);
                         break;
 
                     // 드래그 중이 아님 :: 슬롯 아이템 드래그
                     case false:
-                        PickupItem(eventData, eventManager);    // 아이템 들기 :: 함수 내 에서 좌,우 클릭 분리
+                        ItemsDrag(eventData, eventManager);    // 아이템 드래그 :: 함수 내 에서 좌,우 클릭 분리
                         return;
                 }
                 break;
         }
     }
 
-    // 슬롯에 있는 아이템 드래그
-    private void PickupItem(PointerEventData p_eventData, EventManager p_eventManager)
+    // 아이템 드래그
+    private void ItemsDrag(PointerEventData p_eventData, EventManager p_eventManager)
     {
         DraggingItem draggingItem = p_eventManager.m_DraggingItem.GetComponent<DraggingItem>();
         int PickupItemStackSize = 0;
 
         switch (p_eventData.pointerId)
         {
-            // 좌 클릭 :: 슬롯에 있는 모든 아이템 들기
+            // 좌 클릭 :: 슬롯에 있는 모든 아이템 드래그
             case -1:
                 PickupItemStackSize = this.m_ItemStack.Count;
                 break;
 
-            // 우 클릭 :: 슬롯에 있는 아이템 절반 들기
+            // 우 클릭 :: 슬롯에 있는 아이템 절반 드래그
             case -2:
                 PickupItemStackSize = Mathf.CeilToInt(this.m_ItemStack.Count * 0.5f);
                 break;
@@ -162,12 +165,12 @@ public class Slot : MonoBehaviour, IPointerClickHandler
         for (int i = 0; i < PickupItemStackSize; i++)
             draggingItem.m_ItemStack.Push(this.m_ItemStack.Pop());
 
-        draggingItem.UpdateUI();
-        this.UpdateUI();
+        this.UpdateUI(draggingItem);
+        this.UpdateUI(this);
     }
 
-    // 드래그 중인 아이템 슬롯에 드랍
-    private void DropTheItem(PointerEventData p_eventData, EventManager p_eventManager)
+    // 아이템 슬롯에 드랍
+    private void ItemsDrop(PointerEventData p_eventData, EventManager p_eventManager)
     {
         DraggingItem draggingItem = p_eventManager.m_DraggingItem.GetComponent<DraggingItem>();
         int DropItemStackSize = 0;
@@ -188,10 +191,17 @@ public class Slot : MonoBehaviour, IPointerClickHandler
         for (int i = 0; i < DropItemStackSize; i++)
             this.m_ItemStack.Push(draggingItem.m_ItemStack.Pop());
 
-        draggingItem.UpdateUI();
-        this.UpdateUI();
+        this.UpdateUI(draggingItem);
+        this.UpdateUI(this);
 
         if (draggingItem.m_ItemStack.Count == 0)
             p_eventManager.m_IsDragging = false;
+    }
+
+    // 아이템 스왑 , 드랍
+    private void ItemsSwapOrDrop(PointerEventData p_eventData, EventManager p_eventManager)
+    {
+        DraggingItem draggingItem = p_eventManager.m_DraggingItem.GetComponent<DraggingItem>();
+
     }
 }
